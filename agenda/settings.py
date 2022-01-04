d08a133b2a8fa220afaf69e6c2569a57e49560e7
fernando-borcsik <fernando.borcsik@gmail.com>
2022-01-03 22:30:58 -0300

    Criada tabela de eventos para a agenda

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gdh=%q5&s4ah^53i4y$mcu!8&j&hbgh8j^ei2j(nxgh)rs&us^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
